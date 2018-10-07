922dfd86e0c57e9ec7363f2b6e9d970263d64d74
Nicolas Metallo <nicolas.metallo@nyu.edu>
2018-10-06 23:25:42 -0300

    update custom.py

--- custom.py
+++ custom.py
@@ -125,7 +125,7 @@ class CustomDataset(utils.Dataset):
             # Get the x, y coordinaets of points of the polygons that make up
             # the outline of each object instance. There are stores in the
             # shape_attributes (see json format above)
-            polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            polygons = [r['shape_attributes'] for r in a['regions']]
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
